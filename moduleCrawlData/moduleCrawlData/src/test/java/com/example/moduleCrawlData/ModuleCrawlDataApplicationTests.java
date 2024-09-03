6e8a7388a6efc68e185ba937be9f68aff6e99544
jeikai <98737616+jeikai@users.noreply.github.com>
2024-09-04 00:29:28 +0700

    init

---
+++ moduleCrawlData/moduleCrawlData/src/test/java/com/example/moduleCrawlData/ModuleCrawlDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moduleCrawlData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleCrawlDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
